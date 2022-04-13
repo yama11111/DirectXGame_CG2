3f3e00ed66cb8ccb42baf0f0debaa2008df9b36b
YamanakaRui <k021g1123@g.neec.ac.jp>
2022-04-14 01:23:09 +0900

    DirectXの初期化

--- main.cpp
+++ main.cpp
@@ -69,11 +69,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	MSG msg{}; // メッセージ
 
 	// ----- DirectX 初期化処理 ----- //
+
+#ifdef _DEBUG
+//デバッグレイヤーをオンに
+	ID3D12Debug* debugController;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) 
+	{
+		debugController->EnableDebugLayer();
+	}
+#endif
+	
 	HRESULT result;
 	ID3D12Device* device = nullptr;
 	IDXGIFactory7* dxgiFactory = nullptr;
 	IDXGISwapChain4* swapChain = nullptr;
-	ID3D12CommandAllocator* commandAllocator = nullptr;
+	ID3D12CommandAllocator* cmdAllocator = nullptr;
 	ID3D12GraphicsCommandList* commandList = nullptr;
 	ID3D12CommandQueue* commandQueue = nullptr;
 	ID3D12DescriptorHeap* rtvHeap = nullptr;
@@ -81,30 +91,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// DXGIファクトリー生成
 	result = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
 	assert(SUCCEEDED(result));
-
-	// アダプターの処理
+	// アダプターの列挙用
 	std::vector<IDXGIAdapter4*> adapters;
-	// ここに特定の名前のアダプターが入る
+	// ここに特定の名前を持つアダプターオブジェクトが入る
 	IDXGIAdapter4* tmpAdapter = nullptr;
-
-	// パフォーマンスが高いものから順に、すべてのアダプターを列挙
-	for (UINT i = 0; i < dxgiFactory->EnumAdapterByGpuPreference(i,
-		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
-		IID_PPV_ARGS(&tmpAdapter)) != DXGI_ERROR_NOT_FOUND;
+	// パフォーマンスが高いものから順に、全てのアダプターを列挙
+	for (UINT i = 0;
+		dxgiFactory->EnumAdapterByGpuPreference(i,
+			DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
+			IID_PPV_ARGS(&tmpAdapter)) != DXGI_ERROR_NOT_FOUND;
 		i++)
 	{
 		// 動的配列に追加
 		adapters.push_back(tmpAdapter);
 	}
+
 	// 妥当なアダプタを選別
-	for (size_t i = 0; i < adapters.size(); i++)
+	for (size_t i = 0; i < adapters.size(); i++) 
 	{
 		DXGI_ADAPTER_DESC3 adapterDesc;
-		// アダプターの情報を取得
+		// アダプターの情報取得
 		adapters[i]->GetDesc3(&adapterDesc);
-
 		// ソフトウェアデバイスを回避
-		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) 
 		{
 			// デバイスを採用してループを抜ける
 			tmpAdapter = adapters[i];
@@ -122,12 +131,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	};
 
 	D3D_FEATURE_LEVEL featureLevel;
-
-	for (size_t i = 0; i < _countof(levels); i++)
+	for (size_t i = 0; i < _countof(levels); i++) 
 	{
 		// 採用したアダプターでデバイス生成
-		result = D3D12CreateDevice(tmpAdapter, levels[i], IID_PPV_ARGS(&device));
-		if (result == S_OK)
+		result = D3D12CreateDevice(tmpAdapter, levels[i],
+			IID_PPV_ARGS(&device));
+		if (result == S_OK) 
 		{
 			// デバイスを生成できた時点でループを抜ける
 			featureLevel = levels[i];
@@ -135,24 +144,73 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		}
 	}
 
-	// コマンドアロケータ生成
+	// コマンドアロケータを生成
 	result = device->CreateCommandAllocator(
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		IID_PPV_ARGS(&commandAllocator));
+		IID_PPV_ARGS(&cmdAllocator));
 	assert(SUCCEEDED(result));
-
-	// コマンドリスト生成
+	// コマンドリストを生成
 	result = device->CreateCommandList(0,
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		commandAllocator, nullptr,
+		cmdAllocator, nullptr,
 		IID_PPV_ARGS(&commandList));
 	assert(SUCCEEDED(result));
 
-	// コマンドキュー設定
+	//コマンドキュー設定
 	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
-	// コマンドキュー生成
+	//コマンドキュー生成
 	result = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
 	assert(SUCCEEDED(result));
+
+	// スワップチェーン設定
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	swapChainDesc.Width = 1280;
+	swapChainDesc.Height = 720;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;		  // 色情報の書式
+	swapChainDesc.SampleDesc.Count = 1;						  // マルチサンプルしない
+	swapChainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;		  // バックバッファ用
+	swapChainDesc.BufferCount = 2;							  // バッファ数を2つに設定
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; // フリップ後は破棄
+	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	// スワップチェーン生成
+	result = dxgiFactory->CreateSwapChainForHwnd(
+		commandQueue, hwnd, &swapChainDesc, nullptr, nullptr,
+		(IDXGISwapChain1**)&swapChain);
+	assert(SUCCEEDED(result));
+
+	// デスクリプタヒープ設定
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;		// レンダーターゲットビュー
+	rtvHeapDesc.NumDescriptors = swapChainDesc.BufferCount; // 裏表の2つ
+	// デスクリプタヒープ生成
+	device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
+
+	// バックバッファ
+	std::vector<ID3D12Resource*> backBuffers;
+	backBuffers.resize(swapChainDesc.BufferCount);
+
+	// スワップチェーンの全てのバッファについて処理する
+	for (size_t i = 0; i < backBuffers.size(); i++) 
+	{
+		// スワップチェーンからバッファ取得
+		swapChain->GetBuffer((UINT)i, IID_PPV_ARGS(&backBuffers[i]));
+		// デスクリプタヒープのハンドル取得
+		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
+		// 裏か表かでアドレスがずれる
+		rtvHandle.ptr += i * device->GetDescriptorHandleIncrementSize(rtvHeapDesc.Type);
+		// レンダーターゲットビュー設定
+		D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
+		// シェーダーの計算結果をSRGBに変換して書き込む
+		rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+		rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+		// レンダーターゲットビュー生成
+		device->CreateRenderTargetView(backBuffers[i], &rtvDesc, rtvHandle);
+	}
+
+	// フェンスの生成
+	ID3D12Fence* fence = nullptr;
+	UINT64 fenceVal = 0;
+	result = device->CreateFence(fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
 
 	// ------------------------------ //
 
