6d6c649d5823ab7111da40d5f63bc01ae8af6486
YamanakaRui <k021g1123@g.neec.ac.jp>
2022-06-29 15:07:36 +0900

    ライティング

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -20,6 +20,7 @@ SamplerState smp : register(s0);      // 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return float4(tex.Sample(smp, input.uv)) * color;
+	//return float4(tex.Sample(smp, input.uv)) * color;
+	return float4(input.normal, 1);
 	//return float4(1,1,1,1);	
 }
